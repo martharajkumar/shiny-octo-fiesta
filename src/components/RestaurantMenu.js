8a43c413eb89d8571e4c349601c6bea0ae88ba95
Martha Raj Kumar <martharajkumar116@gmail.com>
2024-04-22 16:14:30 +0530

    created restaurant menu accordion component

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,5 +1,11 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import Accordion from "@mui/material/Accordion";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Typography from "@mui/material/Typography";
 
 const RestaurantMenu = () => {
   const [restaurantMenu, setRestaurantMenu] = useState({});
@@ -18,11 +24,38 @@ const RestaurantMenu = () => {
     setRestaurantMenu(resMenu);
   };
 
-  console.log(restaurantMenu);
-
   return (
     <>
-      <div className="restaurant-menu-container">Menu</div>
+      <div className="restaurant-menu-container">
+        <div className="restauarant-name">
+          {restaurantMenu?.data?.cards[0]?.card?.card?.text}
+        </div>
+        {restaurantMenu?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.map(
+          (menuCard, index) => (
+            <div className="menu-accordion " key={index}>
+              {console.log(menuCard, ":::::::::::menucard")}
+              {menuCard?.card?.card?.title != undefined ? (
+                <Accordion>
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1-content"
+                    id="panel1-header"
+                  >
+                    <Typography>{menuCard?.card?.card?.title}</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Suspendisse malesuada lacus ex, sit amet blandit leo
+                      lobortis eget.
+                    </Typography>
+                  </AccordionDetails>
+                </Accordion>
+              ) : undefined}
+            </div>
+          )
+        )}
+      </div>
     </>
   );
 };
